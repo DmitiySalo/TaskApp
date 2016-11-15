8b7588b8b50123cea0f93d3f8957303c03b5f51f
Salo Dmyto <dmitriyavanty@gmail.com>
2016-11-15 20:27:56 +0200

    update DRF

--- api/Serializers.py
+++ api/Serializers.py
@@ -4,7 +4,17 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Task
-		fields = '__all__'
+		fields = ('id','task_name','task_desc')
 
 
+"""
+class TaskSerializer(serializers.Serializer):
+	id = serializers.ReadOnlyField()
+	task_name = serializers.ReadOnlyField()
+	task_desc = serializers.ReadOnlyField()
+	
+
+	def to_representation(self, data):
+		res = super(TaskSerializer, self).to_representation(data)
+		return {res['id']: res}"""
 
--- api/views.py
+++ api/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.response import Response
 from .models import Task
-from .serializers import TaskSerializer
+from .Serializers import TaskSerializer
 from django.conf.urls import include
-from rest_framework.views import APIView 
-#class TaskViewSet(viewsets.ModelViewSet):
-#	queryset = Task.objects.all().order_by('-date_created')
-#	serializer_class = TaskSerializer
+from rest_framework.response import Response
 
-class TaskList(APIView):
-	def get(self,request):
-		task = Task.objects.all()
-		serializer = TaskSerializer(task, many=True)
-		return Response(serializer.data)
-	def post(self):
-			pass	
+
+class TaskViewSet(viewsets.ModelViewSet):
+	
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    tasks = TaskSerializer(Task.objects.all(), many=True)
+    data = tasks.data[:]
+    #{task['id']: task for task in data}
+
+"""   def retrieve(self, request, *args, **kwargs):
+        return Response({task['id']: task for task in TaskViewSet.data})
+
+    def list(self, request, *args, **kwargs):
+        return Response({task['id']: task for task in TaskViewSet.data})"""
+   
+
